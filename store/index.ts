71fa062f43acc3d08ab0f22a24750115f2120376
Adnan-kreiker <adnankreiker@hotmail.com>
2021-10-25 11:52:52 +0300

    cleaned up the store

--- store/index.ts
+++ store/index.ts
@@ -5,13 +5,9 @@ import { getModule } from 'nuxt-property-decorator';
  * Import, define and export all modules.
  */
 
-import Example from './modules/example';
-import Mountains from './modules/mountains';
 import Products from './modules/products';
 import Leads from './modules/leads';
 
-export let exampleStore: Example;
-export let mountainsStore: Mountains;
 export let productsStore: Products;
 export let leadsStore: Leads;
 
@@ -19,8 +15,6 @@ export let leadsStore: Leads;
  * A Vuex plugin that sets up the imported modules.
  */
 const initializer = (store: Store<any>) => {
-	exampleStore = getModule(Example, store);
-	mountainsStore = getModule(Mountains, store);
 	productsStore = getModule(Products, store);
 	leadsStore = getModule(Leads, store);
 };
